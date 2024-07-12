a14e9c5f6641a1c1a4d219427b28895266a262a9
Nicholas Ung <nicholasung22@gmail.com>
2024-07-12 12:32:15 -0700

    Update image.h

--- include/image.h
+++ include/image.h
@@ -20,23 +20,24 @@ typedef struct
 
 /* Function prototypes for image operations */
 Image *image_create(int rows, int cols);
-void image_free(Image *img);
-void image_alloc(Image *img, int rows, int cols);
-void image_dealloc(Image *img);
-void image_reset(Image *img);
-FPixel image_getf(Image *img, int row, int col);
-float image_getc(Image *img, int row, int col);
-float image_geta(Image *img, int row, int col);
-float image_getz(Image *img, int row, int col);
-void image_setf(Image *img, int row, int col, FPixel value);
-void image_setc(Image *img, int row, int col, int ch, float value);
-void image_seta(Image *img, int row, int col, float val);
-void image_setz(Image *img, int row, int col, float val);
-void image_fill(Image *img, FPixel value);
-void image_fillrgb(Image *img, float r, float g, float b);
-void image_filla(Image *img, float a);
-void image_fillz(Image *img, float z);
+void image_free(Image *src);
+void image_init(Image *src); 
+int image_alloc(Image *src, int rows, int cols);
+void image_dealloc(Image *src);
+void image_reset(Image *src);
+FPixel image_getf(Image *src, int row, int col);
+float image_getc(Image *src, int row, int col);
+float image_geta(Image *src, int row, int col);
+float image_getz(Image *src, int row, int col);
+void image_setf(Image *src, int row, int col, FPixel value);
+void image_setc(Image *src, int row, int col, int ch, float value);
+void image_seta(Image *src, int row, int col, float val);
+void image_setz(Image *src, int row, int col, float val);
+void image_fill(Image *src, FPixel value);
+void image_fillrgb(Image *src, float r, float g, float b);
+void image_filla(Image *src, float a);
+void image_fillz(Image *src, float z);
 Image *image_read(char *filename);
-int image_write(Image *img, char *filename);
+int image_write(Image *src, char *filename);
 
 #endif
